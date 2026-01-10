9947acc04aa04d3da3ecef1f711995f882926608
Unilove's BS <zuberpawel08@gmail.com>
2026-01-11 00:51:53 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# 5_sprawozdan_w_tesli
+5 Koszmarnych Nocy W Tesli
---
+++ Skrypty/kamera.gd
@@ -0,0 +1,36 @@
+extends Camera2D
+enum {NoCam, Cam}
+var SmoothingType := Cam
+@onready
+var Kamera = $"."
+var CamPosition := 0.0
+var CamSpeed := 200
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if SmoothingType == Cam && abs(Kamera.position.x - CamPosition) > 5:
+		Kamera.position_smoothing_enabled = false
+		if Kamera.position.x > CamPosition:
+			Kamera.position.x -= CamSpeed * delta
+		elif Kamera.position.x < CamPosition:
+			Kamera.position.x += CamSpeed * delta
+	else:
+		Kamera.position_smoothing_enabled = true
+
+func _input(event):
+	if event is InputEventMouseMotion:
+		var TruePosition = clamp(event.position.x, 0, 1280) / 2
+		if SmoothingType == NoCam:
+			Kamera.position_smoothing_enabled = true
+			Kamera.position.x = TruePosition - 320
+		else:
+			CamPosition = TruePosition - 320
+			
+			
+			
+		
+		
